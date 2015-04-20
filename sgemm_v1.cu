f239c13fea537ff9da31d7c520c145fd954103c7
syberslidder <syberslidder@gmail.com>
2015-04-20 15:44:52 -0500

    Modified the GEMM kernel

--- sgemm_v1.cu
+++ sgemm_v1.cu
@@ -13,7 +13,7 @@ using namespace std;
 #define THREADS_PER_BLOCK 8*8
 #define BLOCKS 64
 
-
+/*
 __global__ void calcSquareSumVector(float *srcMatrix,
                                     float *sqSumVector,
                                     int    M,
@@ -51,7 +51,7 @@ __global__ void calcSquareSumVector(float *srcMatrix,
 
 
 }
-
+*/
 
 __global__ void combinedSGEMM(
        float * _A, // Global pointer to matrix A 
@@ -71,46 +71,53 @@ __global__ void combinedSGEMM(
     // Sub block of A of size THREADS_PER_BLOCK_Y by KSub
     __shared__ float A_Holder[THREADS_PER_BLOCK];
     // Sub block of B of size KSub by THREADS_PER_BLOCK_X
-    __shared__ float B_holder[THREADS_PER_BLOCK];
+    __shared__ float B_Holder[THREADS_PER_BLOCK];
     
     // Some identity information
-    int localX_idx  = (threadIdx.x % THREADS_PER_BLOCK_X);
-    int localY_idx  = (threadIdx.y % THREADS_PER_BLOCK_Y);
-    
     // myID is a linear index for shared memory
-    int myID  = localX_idx + (THREADS_PER_BLOCK_X)*localY_idx;  
-    int A_row = (blockIdx.y*THREADS_PER_BLOCK_Y) + ;
-
+    int myID  = blockIdx.x + (blockDim.x)*threadIdx.y;      
+    
+    // Does not change
+    int A_row    = (blockIdx.y*blockDim.y) + threadIdx.y;
+    int A_column = threadIdx.x;
+    int B_row    = threadIdx.y;
+    // Does not change
+    int B_column = (blockIdx.x*blockDim.x) + threadIdx.x;
+    
     // This threads value of C
     float temp[4]  = {0.0f,0.0f,0.0f,0.0f};
     
     // Load in sublocks of A and B into shared memory
-    for (int i = 0; i < K; i += THREADS_PER_BLOCK_X) {
+    for (int i = 0; i < K; i += blockDim.x) {
         // Each thread loads in one element from A and one Element from B
-        A_Holder[myID] = A[];
-        B_Holder[myID] = B[]; 
+        A_Holder[myID] = _A[A_row*K+A_column];
+        B_Holder[myID] = _B[B_row*N+B_column]; 
     
         __syncthreads();
     
         // Calculate the partial sum
-        int idxA = localX_idx * THREADS_PER_BLOCK_X;
-        int idxB = localY_idx * THREADS_PER_BLOCK_Y;
-        for (int j = 0; j < THREADS_PER_BLOCK_X; j += 4) {
+        int idxA = A_row % blockDim.x;
+        int idxB = B_column % blockDim.y;
+        for (int j = 0; j < blockDim.x; j += 4) {
             temp[0] += A_Holder[idxA+0]*B_Holder[idxB+0];
             temp[1] += A_Holder[idxA+1]*B_Holder[idxB+1];
             temp[2] += A_Holder[idxA+2]*B_Holder[idxB+2];
             temp[3] += A_Holder[idxA+3]*B_Holder[idxB+3];
+	    idxA++;
+	    idxB++;
         }
     
         __syncthreads();
     
         // Update indexing information
-    
+	A_column += blockDim.x;
+	B_row    += blockDim.y;
+	
     }
 
     temp[0] += temp[1] + temp[2] + temp[3];
     // We have Cij at this point
-
+    _C[(blockIdx.y*blockDim.y*N)+(blockIdx.x*blockDim.x)+threadIdx.x] = temp[0];
 
 }
 
@@ -189,7 +196,7 @@ int main(int argc, char * argv[]) {
             hostA[m*K+k] = (float)rand()/(float)(RAND_MAX/1000);
         }
     }
-    for(int k = 0; i < K; k++){
+    for(int k = 0; k < K; k++){
         for(int n = 0; n < N; n++){
             hostA[k*N*+n] = (float)rand()/(float)(RAND_MAX/1000);
         }
@@ -216,7 +223,7 @@ int main(int argc, char * argv[]) {
     
     
     // Launch kernel
-    combinedSGEMM<<<gridSize,blockSize>>>();
+    combinedSGEMM<<<gridSize,blockSize>>>(devA,devB,devC,0,0,M,N,K);
     
     // Transfer result from device to host
     cudaMemcpy(hostC,devC,M*N*sizeof(float),cudaMemcpyDeviceToHost);
