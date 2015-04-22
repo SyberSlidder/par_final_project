960eb6a7db14d1884a092bca4a77de8c51a83698
lca <lca@archer-blades08.ece.utexas.edu>
2015-04-21 23:34:59 -0500

    only shared memory, performance slightly improve

--- texture_dgemm.cu
+++ texture_dgemm.cu
@@ -17,43 +17,62 @@ float warpReduceSum(float val) {
 __global__
 void tileMul(float *_A, float *_B, float *_C,int m, int n, int k, int bm, int bn, int bk, int KtileWidth,int threadPerBlock){
         //assume A is row-major and B is column-major and C has initial value all zero
-	int id = threadIdx.x;
+	int idx = threadIdx.x;
+	int idy = threadIdx.y;
 	int blockx = blockIdx.x;
 	int blocky = blockIdx.y;
 	int blockz = blockIdx.z;
-	float value;
-	int l,loop;
+	float value[2][2];
+	float _value;
+	int l,loopi,loopj;
 	int tileNum,currentRow;
-    	int idMod32 = (id%warpSize);
-    	int idDivide32 = (id/warpSize);
+	int Comb = 4; //float4
+	int half = threadPerBlock/2;
+	int idy_half = idy-half;
+	int idModbm = idy%(bm/2);
+	int idDividebm = idy/(bm/2);
+    	//int idMod32 = (id%warpSize);
+    	//int idDivide32 = (id/warpSize);
 	//int subtileN = threadPerBlock/warpSize;
 	int totTileNum = min((KtileWidth+bk-1)/bk,(k+bk-1)/bk);
-	//float *A = _A + blockx * bm*k;
-	float *B = _B + blocky * bn*k;
-	//float *C = _C + blockx * bm*n + blocky * bn;
+	float *A = _A + blocky * bm*k;
+	float *B = _B + blockx * bn*k;
+	//float *C = _C + blocky * bm*n + blockx * bn;
 	
 //	use share memory
-	__shared__ float4 tileB[32][8]; //bk*bn
+	__shared__ float4 tileA[8][16]; //bk*bn
+	__shared__ float4 tileB[8][16]; //bk*bn
 	__shared__ float tempC[8][8]; //bm*bn,FIXME:dynamic allocate tempC[bn][bm]
 
 	for(tileNum=0 ; tileNum < totTileNum; tileNum++){
 		/* tileC(blockx,blocky) += tileA[i] * tileB[i]*/
-		int index = (blockz*KtileWidth + tileNum*bk + idMod32)<<2;
+		int index = blockz*KtileWidth + tileNum*bk + idx*Comb;
 		if(index <= k){
-            		tileB[idMod32][idDivide32] = reinterpret_cast<float4*> (B)[idDivide32 * k + index>>2];
-			//printf("id=%d,x=%f,y=%f,z=%f,w=%f,B[%d][%d]\n",id,tileB[idMod32][idDivide32].x,tileB[idMod32][idDivide32].y,tileB[idMod32][idDivide32].z,tileB[idMod32][idDivide32].w,idDivide32,index);
+            		if(idy<half) tileB[idy][idx] = reinterpret_cast<float4*> (B)[(idy*k + index)/Comb];
+					//printf("id=(%d,%d),x=%f,y=%f,z=%f,w=%f,B[%d][%d],offset=%d\n",idx,idy,tileB[idy][idx].x,tileB[idy][idx].y,tileB[idy][idx].z,tileB[idy][idx].w,idy,idx,(idy*k + index)/Comb);}
+            		else tileA[idy_half][idx] = reinterpret_cast<float4*> (A)[((idy_half)*k + index)/Comb];
+					//printf("id=(%d,%d),x=%f,y=%f,z=%f,w=%f,A[%d][%d],offset=%d\n",idx,idy_half,tileA[idy_half][idx].x,tileA[idy_half][idx].y,tileA[idy_half][idx].z,tileA[idy_half][idx].w,idy_half,idx,(idy_half*k + index)/Comb);}
         	}
-		for(currentRow=0; currentRow < bm; currentRow++){
+		//for(currentRow=0; currentRow < bm; currentRow++){
 			//float elementA = A[k*currentRow + blockz*KtileWidth + tileNum*bk + idMod32];//put elementA in reg	
 			//for(loop=0; (loop*subtileN)<bn; loop++){
 				if(index <= k){
-					int offset =( ((blocky*bm + currentRow)*k + blockz*KtileWidth + tileNum*bk) >> 2 )+ idMod32;
-					float4 a = tex1Dfetch(texture_A, offset);
-					float4 b = tileB[idMod32][idDivide32];
-					value = a.x*b.x + a.y*b.y + a.z*b.z + a.w*b.w;
+					float4 a0 = tileA[idDividebm*2][idx];
+					float4 a1 = tileA[idDividebm*2+1][idx];
+					float4 b0 = tileB[idModbm*2][idx];
+					float4 b1 = tileB[idModbm*2+1][idx];
+					value[0][0] = a0.x*b0.x + a0.y*b0.y + a0.z*b0.z + a0.w*b0.w;
+					value[0][1] = a0.x*b1.x + a0.y*b1.y + a0.z*b1.z + a0.w*b1.w;
+					value[1][0] = a1.x*b0.x + a1.y*b0.y + a1.z*b0.z + a1.w*b0.w;
+					value[1][1] = a1.x*b1.x + a1.y*b1.y + a1.z*b1.z + a1.w*b1.w;
 					//printf("index=%d,value=%f,offset=%d\n",index,value,offset);
 				}
-				else value = 0;
+				else{	
+					 value[0][0] = 0;
+					 value[0][1] = 0;
+					 value[1][0] = 0;
+					 value[1][1] = 0;
+				}
 			/*if(currentRow == 0) printf("Index=%d.block tileNum=%d. Thread %d->%d.Value %f * %f= %f\n",index,tileNum,id,idMod32,elementA, tex1Dfetch(texture_B, (blocky*bn + loop*subtileN + idDivide32)*k + idMod32),value);
 			  if(currentRow == 0){
 				float valueb = tex1Dfetch(texture_B, (blocky*bn + loop*subtileN + idDivide32)*k + idMod32);
@@ -61,30 +80,36 @@ void tileMul(float *_A, float *_B, float *_C,int m, int n, int k, int bm, int bn
 			  }
 			*/
         	    //Shuffle Warp Reduce 
-        	    for (l=warpSize/2; l>=1; l/=2)
-        	        value += __shfl_down(value, l);
+		for(loopi=0;loopi<2;loopi++)
+			for(loopj=0;loopj<2;loopj++){
+				_value=value[loopi][loopj];	
+        	    for (l=warpSize/2; l>=2; l/=2)
+        	        _value += __shfl_down(_value, l);
 	      	    //if(currentRow == 0) printf("Thread %d final value = %f\n", id, value);
-        	    if(idMod32 == 0) {
+        	    if(idx == 0) {
 					//tempC[currentRow][idDivide32+loop*32] +=value;		
-					tempC[currentRow][idDivide32] +=value;		
+					tempC[idDividebm*2+loopi][idModbm*2+loopj] += _value;		
 					//printf("C_temp[%d][%d] final value = %f\n",currentRow,idDivide32+loop*subtileN,value);		    	
 					if(tileNum == totTileNum-1){
 						//_C[ (currentRow+blockx*bm)*n + blocky * bn + idDivide32+loop*32] = tempC[currentRow*bn + idDivide32+loop*32];
-						atomicAdd(&_C[ (currentRow+blockx*bm)*n + blocky*bn + idDivide32] , tempC[currentRow][idDivide32]);
+						atomicAdd(&_C[ (idDividebm*2+loopi+blocky*bm)*n + blockx*bn + idModbm*2+loopj] , tempC[idDividebm*2+loopi][idModbm*2+loopj]);
 						//if(currentRow == 0) printf("C[%d][%d] final value = %f\n",currentRow+blockx*bm,idDivide32+loop*32+blocky*bn,tempC[currentRow*bn + idDivide32+loop*32]);
 					}
     			}
+			}
 			//}
-		}
+		//}
 	}
 }
 int main(int argc, char* argv[]){
 	int m,n,k;
 	int i,j;
 //	m=4096; n=4096; k=4096;
+	int microTile = 8;
 	sscanf( argv[ 1 ], "%d", &m );
 	sscanf( argv[ 2 ], "%d", &n );
 	sscanf( argv[ 3 ], "%d", &k );
+	sscanf( argv[ 4 ], "%d", &microTile );
 	float *A = (float*)malloc(sizeof(float)*m*k);
 	float *B = (float*)malloc(sizeof(float)*k*n);
 	float *C = (float*)malloc(sizeof(float)*m*n);
@@ -145,17 +170,16 @@ int main(int argc, char* argv[]){
 	//cudaBindTexture(NULL,texture_B,dev_B,k*n*sizeof(float));
 	int bm=8;
 	int bn=8;//32*4
-	int bk=128;
+	int bk=64;
 	//int texCache = 48*1024; //48K
 	//int microTile = texCache/sizeof(float)/bn/bk;//12
-	int microTile = 16;
-	int threadPerBlock = 256;
+	int threadPerBlock = 16;
 	int N = (n+bn-1)/bn;//gridX
 	int M = (m+bm-1)/bm;//gridY
 	int KtileWidth = bk*microTile;//12*32/2
 	int K = (k+KtileWidth-1)/KtileWidth;//gridZ
 	dim3 grid_dim(N,M,K);
-	dim3 block_dim(threadPerBlock,1,1);
+	dim3 block_dim(threadPerBlock,threadPerBlock,1);
 	int blockNum = M*N*K;
 	printf("block number %d*%d*%d= %d\n",N,M,K,blockNum);
 	double cpuStartTime = CycleTimer::currentSeconds();
